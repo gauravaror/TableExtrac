2898c36aff7d05c00cf19b827b6fa7a760cbb738
Gaurav Arora <gauravarora.daiict@gmail.com>
2015-06-12 14:13:49 +0530

    merging lines with two columns and one column with one character

--- tableextractor.cc
+++ tableextractor.cc
@@ -1,3 +1,4 @@
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -79,6 +80,84 @@ void update_block(TableBlock & mlb, Line  l) {
   mlb.max_elements = std::max(mlb.max_elements,int(l.texts->size()));
 }
 
+bool check_mergable_columns(std::vector<TextElement *> * & textElement) {
+  //std::cout<<" vvv " <<textElement->size()<<" "<<textElement->at(0)->value<<" "<<textElement->at(0)->value.size()<<endl;
+  if ((textElement->size() == 2) && (textElement->at(0)->value.size() == 2)) {
+    TextElement* tel =     textElement->at(0);
+    TextElement* tel1 =     textElement->at(1);
+    tel->value += " ";
+    tel->value += tel1->value;
+    tel->right = tel1->right;
+    textElement->pop_back();
+    return true;
+  }
+  return false;
+}
+
+bool line_mergable(Line &curr_l,TableBlock& curr_bl) {
+  bool mergable = true;
+  Line prev_line = lines->at(curr_bl.end);
+
+  //Check if the colum is mergable
+  for(int curr_itr = 0;curr_itr < curr_l.texts->size();curr_itr++) {
+    bool element_mergable = false;
+    for(int curr_itr_blk = 0;curr_itr_blk < prev_line.texts->size();curr_itr_blk++) {
+      TextElement *mergable_element = curr_l.texts->at(curr_itr);
+      TextElement *test_element_curr = prev_line.texts->at(curr_itr_blk);
+     // std::cout<<test_element_curr->value<<" itr value  "<<curr_itr_blk<<" "<<mergable_element->value<<" "<<mergable_element->left<<" left rights "<<(test_element_curr->left -(test_element_curr->height/2))<<endl;
+
+      if(prev_line.texts->size() -1 != curr_itr_blk) {
+        TextElement* test_element_next = prev_line.texts->at(curr_itr_blk+1);  
+      //  std::cout<<"  rigg  " <<mergable_element->right << "  "<<test_element_next->right<<"  "<<(test_element_next->left-(test_element_next->height/2))<<endl;
+        if( (mergable_element->left > (test_element_curr->left -(test_element_curr->height/2))) && (mergable_element->right < (test_element_next->left-(test_element_next->height/2)))) {
+          element_mergable= true;
+          //cout<<"merding"<<endl;
+        }
+      } else {
+        if( (mergable_element->left > (test_element_curr->left -(test_element_curr->height/2)))) {
+          element_mergable = true;
+         // cout<<"merging"<<endl;
+        }
+      }
+    }
+   // std::cout<<" mergable_element "<<element_mergable<<endl;
+    mergable &= element_mergable;
+  }
+    //Execute the merge
+    if(mergable == true) {
+    for(int curr_itr = 0;curr_itr < curr_l.texts->size();curr_itr++) {
+    for(int curr_itr_blk = 0;curr_itr_blk < prev_line.texts->size();curr_itr_blk++) {
+      TextElement *mergable_element = curr_l.texts->at(curr_itr);
+      TextElement *test_element_curr = prev_line.texts->at(curr_itr_blk);
+   //   std::cout<<test_element_curr->value<<" itr value  "<<mergable_element->value<<" "<<mergable_element->left<<" left rights "<<(test_element_curr->left -3)<<endl;
+
+      if(prev_line.texts->size() - 1 != curr_itr_blk) {
+        TextElement* test_element_next = prev_line.texts->at(curr_itr_blk+1);  
+        if( (mergable_element->left > (test_element_curr->left -(test_element_curr->height/2))) && (mergable_element->right < (test_element_next->left-(test_element_next->height/2)))) {
+          test_element_curr->value += "  ";
+          test_element_curr->value += mergable_element->value;
+          test_element_curr->left = std::min(test_element_curr->left,mergable_element->left);
+          test_element_curr->right = std::max(test_element_curr->right,mergable_element->right);
+    //            std::cout<<test_element_curr->value<<" value  "<<mergable_element->value<<endl;
+
+        }
+      } else {
+        if( (mergable_element->left > (test_element_curr->left -(test_element_curr->height/2)))) {
+          test_element_curr->value += "  ";
+          test_element_curr->value += mergable_element->value;
+          test_element_curr->left = std::min(test_element_curr->left,mergable_element->left);
+          test_element_curr->right = std::max(test_element_curr->right,mergable_element->right);
+  //              std::cout<<test_element_curr->value<<" value  "<<mergable_element->value<<endl;
+
+        }
+      }
+    }
+  }
+  }
+
+  return mergable;
+}
+
 int
 main(int argc, char* argv[])
 {
@@ -129,12 +208,25 @@ main(int argc, char* argv[])
     bool multi_modus = false;        
     //std::cout <<lines->size()<<" lines size ";
     for(unsigned int i = 0; i < lines->size();i++) {
-     // std::cout<<endl;
+      //std::cout<<endl;
       Line curr = lines->at(i);
       sort(curr.texts->begin(),curr.texts->end(),left_comparator);
+      //std::cout<< check_mergable_columns(curr.texts)<<" merged colum "<<curr.texts->at(0)->value<<endl;
+      check_mergable_columns(curr.texts);
       if (curr.texts->size() > 1) {
         if (multi_modus == true) {
           TableBlock curr_block = tblock->back();
+          Line prev_line = lines->at(curr_block.end);
+          if( prev_line.texts->size() > curr.texts->size()) {
+            bool merged = line_mergable(curr,curr_block);
+          //  cout<<merged<<" merged  "<<endl;
+            if(merged) {
+              lines->erase(lines->begin()+i);
+              i--;
+              continue;
+            }
+
+          }
       //    cout<<"update_block  "<<curr.leftmost<<endl;
           update_block(curr_block,curr);
           //cout<<"update_block  end "<<curr_block.end<<endl;
@@ -151,7 +243,18 @@ main(int argc, char* argv[])
       } else if (curr.texts->size() == 1){
         if (multi_modus == true) {
           TableBlock curr_block = tblock->back();
-          if((curr.leftmost >= (curr_block.leftmost-3) ) ) {
+          if((curr.leftmost >= (curr_block.leftmost-3) )) {
+            bool merged_ = line_mergable(curr,curr_block);
+            //cout<<merged_<<" merged_  "<<endl;
+            if(merged_) {
+              lines->erase(lines->begin()+i);
+              i--;
+              continue;
+            } else {
+              multi_modus = false;
+              continue;
+            }
+
             //cout<<"update_block  "<<curr_block.end<<endl;
             update_block(curr_block,curr);
             //cout<<"update_block  end "<<curr_block.end<<endl;
@@ -165,9 +268,9 @@ main(int argc, char* argv[])
         }
 
       }
-      /*for(std::vector<TextElement *>::iterator itt = curr.texts->begin();itt!=curr.texts->end();++itt) {
+     /* for(std::vector<TextElement *>::iterator itt = curr.texts->begin();itt!=curr.texts->end();++itt) {
         std::cout<<(*itt)->value <<"("<<(*itt)->left<<")      ";
-      }    */
+      }  */  
     }
     std::cout <<tblock->size()<<" block size ";
     for(unsigned int i = 0; i < tblock->size();i++) {
@@ -178,14 +281,14 @@ main(int argc, char* argv[])
             std::cout<<endl;
             Line curr = lines->at(l);
             for(std::vector<TextElement *>::iterator itt = curr.texts->begin();itt!=curr.texts->end();++itt) {
-              std::cout<<(*itt)->left <<"("<<(*itt)->right<<")      ";
+              std::cout<<(*itt)->value<<"  "<<(*itt)->left <<"("<<(*itt)->right<<")      ";
             }     
         }
 
        }
     
-    /*
-    for(std::vector<Line>::iterator itl = lines->begin(); itl != lines->end(); ++itl) {
+    
+    /*for(std::vector<Line>::iterator itl = lines->begin(); itl != lines->end(); ++itl) {
       std::cout<<endl;
       sort(itl->texts->begin(),itl->texts->end(),left_comparator);
       for(std::vector<TextElement *>::iterator itt = itl->texts->begin();itt!=itl->texts->end();++itt) {
